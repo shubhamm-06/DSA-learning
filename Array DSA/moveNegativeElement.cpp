748109f7c352b61a053b5d57faaf7cca06e2285b
Shubham Kumar Singh <shubhamkrsingh06@gmail.com>
2022-07-02 17:16:23 +0530

    update

--- Array DSA/moveNegativeElement.cpp
+++ Array DSA/moveNegativeElement.cpp
@@ -1,14 +1,11 @@
 // Q - Move all negative elements to end
 // Given an unsorted array arr[] of size N having both negative and positive integers. The task is place all negative element at the end of array without changing the order of positive element and negative element.
-// Brute force approch 
-// C++ program to Move All -ve Element At End
-// Without changing order Of Array Element
+
 #include<bits/stdc++.h>
 using namespace std;
 void rearrangeElements(int arr[], int n)
 {
 	int temp[n];
-	int j = 0; // index of temp
 	for (int i = 0; i < n ; i++)
 		if (arr[i] >= 0 )
 			temp[j++] = arr[i];
